3a6780f28dbdc3e6587b2aa914e003d36e3b4d16
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-08-19 19:59:07 +0900

    Add Task migrate factory

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class TaskFactory extends Factory
+class TodoFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -14,10 +14,7 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->realText(rand(15,40)),
-            'is_done'=>$this->faker->boolean(10),
-            'created_at'=>now(),
-            'updated_at'=>now()
+            //
         ];
     }
 }
--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Task;
 
-class TaskSeeder extends Seeder
+class TodoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +13,6 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        Task::factory()->count(10)->create();
+        //
     }
 }
